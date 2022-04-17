39024aef341da84ea6a69461eb4e65cc59f1e068
Peter-Kung <410621208@gms.ndhu.edu.tw>
2022-04-17 22:10:31 +0800

    Add reverse function and remove function

--- queue.c
+++ queue.c
@@ -25,14 +25,14 @@ struct list_head *q_new()
 /* Free all storage used by queue */
 void q_free(struct list_head *l)
 {
-    struct list_head *cur = NULL;
-    while (l) {
-        cur = l;
-        l = l->next;
-        element_t *node = container_of(cur, element_t, list);
-        free(node->value);
-        free(node);
+    if (!l)
+        return;
+    element_t *cur = NULL, *next;
+    list_for_each_entry_safe (cur, next, l, list) {
+        free(cur->value);
+        free(cur);
     }
+    free(l);
 }
 
 /* Insert an element at head of queue */
@@ -72,19 +72,39 @@ element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
     if (list_empty(head))
         return NULL;
 
-    return NULL;
+    struct list_head *cur = head->next;
+    element_t *ele = container_of(cur, element_t, list);
+    head->next = cur->next;
+    cur->next->prev = head;
+    cur->next = NULL;
+    cur->prev = NULL;
+    return ele;
 }
 
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (list_empty(head))
+        return NULL;
+
+    struct list_head *cur = head->prev;
+    element_t *ele = container_of(cur, element_t, list);
+    head->prev = cur->prev;
+    cur->prev->next = head;
+    cur->prev = cur->next = NULL;
+    return ele;
 }
 
 /* Return number of elements in queue */
 int q_size(struct list_head *head)
 {
-    return -1;
+    if (!head)
+        return -1;
+    int count = 0;
+    struct list_head *node;
+    list_for_each (node, head->next)
+        count++;
+    return count;
 }
 
 /* Delete the middle node in queue */
@@ -108,7 +128,29 @@ void q_swap(struct list_head *head)
 }
 
 /* Reverse elements in queue */
-void q_reverse(struct list_head *head) {}
+void q_reverse(struct list_head *head)
+{
+    struct list_head *curHead = head->next;
+    struct list_head *curTail = head->prev;
+    struct list_head *list_reverse(struct list_head *);
+    curTail->next = NULL;
+    list_reverse(curHead);
+
+    head->next = curTail;
+    head->prev = curHead;
+    curTail->prev = head;
+    curHead->next = head;
+}
+
+struct list_head *list_reverse(struct list_head *cur)
+{
+    if (!cur || !cur->next)
+        return cur;
+    struct list_head *next = list_reverse(cur->next);
+    cur->prev = next;
+    next->next = cur;
+    return cur;
+}
 
 /* Sort elements of queue in ascending order */
 void q_sort(struct list_head *head) {}
